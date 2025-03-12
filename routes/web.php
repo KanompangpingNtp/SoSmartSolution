0a4317ee4ee99747a6e3fddca34ce5adb7ee4b3b
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-03-12 10:15:23 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home-page.index');
-});
+Route::get('/', [PagesController::class, 'Index'])->name('Index');
